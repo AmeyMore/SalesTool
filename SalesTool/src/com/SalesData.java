d2a29b43619d6e00457454f36afe876d1646a390
ABHAY TIWARI <atiwari@asicentral.com>
2015-01-06 17:05:00 +0530

    asd

--- SalesTool/src/com/SalesData.java
+++ SalesTool/src/com/SalesData.java
@@ -5,7 +5,10 @@ public class SalesData {
 	
 	
 	public void display(){
-		System.out.println("In SalesData1");
+		System.out.println("In SalesData");
+		System.out.println("In fcap");
+		System.out.println("In small letters");
+		System.out.println("I am in feature one1`");
 		
 	}
 }		
